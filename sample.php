eb9026b75d5a4ecf3db9857d9533674e9813abea
Raja1 <Raja1@gmail.com>
2024-03-12 20:15:58 +0530

    2nd modified for main

--- sample.php
+++ sample.php
@@ -7,7 +7,11 @@ ECHO "Hello Raja <br>";
 Echo "Hello Ram!<br>";
 EcHo "Hello charu!<br>";
 <<<<<<< HEAD
+<<<<<<< HEAD
 Echo "Welcome to our devops class<br?";
+=======
+Echo "Welcome to our class<br?";
+>>>>>>> 1cb7d95 (2nd modified for main)
 =======
 Echo "Welcome to our class<br?";
 >>>>>>> 1cb7d95 (2nd modified for main)
